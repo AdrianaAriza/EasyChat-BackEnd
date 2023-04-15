69cedace1a593b7221ba35a2bcd898afdc933f69
AdrianaAriza <55102581+AdrianaAriza@users.noreply.github.com>
2023-04-15 01:21:52 -0400

    [Feat: socket chat]

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from controller import user, auth
 from config import config
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from typing import List
 
 app = FastAPI()
 app.config = config
@@ -18,3 +20,42 @@ app.include_router(user.router, prefix=f"/user")
 app.include_router(auth.router, prefix=f"/auth")
 
 
+# chat manager
+# Code adapted from https://medium.com/@ahtishamshafi9906/how-to-build-a-simple-chat-application-in-fastapi-7bafad755654
+class SocketManager:
+    def __init__(self):
+        self.active_connections: List[(WebSocket, str)] = []
+
+    async def connect(self, websocket: WebSocket, user: str):
+        await websocket.accept()
+        self.active_connections.append((websocket, user))
+
+    def disconnect(self, websocket: WebSocket, user: str):
+        self.active_connections.remove((websocket, user))
+
+    async def broadcast(self, data):
+        for connection in self.active_connections:
+            await connection[0].send_json(data)
+
+
+manager = SocketManager()
+
+
+@app.websocket("/api/chat")
+async def chat(websocket: WebSocket):
+    sender = websocket.cookies.get("user")
+    if sender:
+        await manager.connect(websocket, sender)
+        response = {
+            "sender": sender,
+            "message": "got connected"
+        }
+        await manager.broadcast(response)
+        try:
+            while True:
+                data = await websocket.receive_json()
+                await manager.broadcast(data)
+        except WebSocketDisconnect:
+            manager.disconnect(websocket, sender)
+            response['message'] = "left"
+            await manager.broadcast(response)
